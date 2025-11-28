e6c621a18bedc85d76a96f0914fa737fb44cbe38
Abhishek Thakur <abhishek626362@gmail.com>
2025-11-28 15:33:16 +0530

    delete functnality

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <script setup lang="ts">
 import Todos from './components/Todos.vue';
-
 import { Authenticator } from "@aws-amplify/ui-vue";
 import "@aws-amplify/ui-vue/styles.css";
 </script>
 
 <template>
-  <main>
-    <Todos />
-        
-    <authenticator>
-      <template v-slot="{ signOut }">
-        <Todos />
-        <button @click="signOut">Sign Out</button>
-      </template>
-    </authenticator>
-  </main>
+  <Authenticator v-slot="{ signOut }">
+    <main>
+      <Todos />
+      <button @click="signOut">Sign Out</button>
+    </main>
+  </Authenticator>
 </template>
-
